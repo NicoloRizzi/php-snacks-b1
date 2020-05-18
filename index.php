a26d51745f938433e7e106507c3992f4622c5d4e
Nicolo <nicolorizzi1996@gmail.com>
2020-05-18 15:50:51 +0200

    JSNACK1

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+  $matches = [
+    [
+      'home' => 'Los Angeles Lakers',
+      'away' => 'Miami Heat',
+      'point_Home' => rand(1, 130),
+      'point_Away' => rand(1, 130)
+    ],
+    [
+      'home' => 'Indiana Pacers',
+      'away' => 'San Antonio Spurs',
+      'point_Home' => rand(1, 130),
+      'point_Away' => rand(1, 130)
+    ],
+    [
+      'home' => 'Los Angeles Clippers',
+      'away' => 'New York Knicks',
+      'point_Home' => rand(1, 130),
+      'point_Away' => rand(1, 130)
+    ],
+    [
+      'home' => 'Philadelphia 76ers',
+      'away' => 'Chicago Bulls',
+      'point_Home' => rand(1, 130),
+      'point_Away' => rand(1, 130)
+    ]
+  ];
+  ?>
+  <h1>Risultati giornata</h1>
+      <ul>
+          <?php for($i=0; $i < count($matches); $i++){ ?>
+              <li>
+                  <?php echo $matches[$i]['home']?>&ndash;
+                      <?php echo $matches[$i]['away']?> |
+                          <?php echo $matches[$i]['point_Home']?> &ndash;
+                              <?php echo $matches[$i]['point_Away']?>
+              </li>
+          <?php } ?>
+      </ul>
+</body>
+</html>
